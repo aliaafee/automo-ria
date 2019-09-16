97899976b43aaa32ef8f6d0ce1f6f047da8b052f
= <ali.aafee@gmail.com>
2019-09-16 23:44:36 +0545

    Added bootstrap, jquery and popper

--- automo-client-el/app/main.js
+++ automo-client-el/app/main.js
@@ -1,49 +1,54 @@
-const { app, BrowserWindow, ipcMain} = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
 
 let mainWindow = null; // #A
 let loginWindow = null;
 
 app.on('ready', () => {
-  console.log('Hello from Electron.');
-  mainWindow = new BrowserWindow({
-    webPreferences: {nodeIntegration: true},
-    show: false
-  });
-  mainWindow.webContents.loadURL(`file://${__dirname}/index.html`); // #A
-  mainWindow.webContents.openDevTools();
+    console.log('Hello from Electron.');
+    mainWindow = new BrowserWindow({
+        webPreferences: { nodeIntegration: true },
+        show: false
+    });
+    mainWindow.webContents.loadURL(`file://${__dirname}/index.html`); // #A
+    mainWindow.webContents.openDevTools();
 
-  loginWindow = new BrowserWindow({
-    webPreferences: {nodeIntegration: true},
-    parent: mainWindow, modal: true,
-    width:400, height:245,
-    frame: false,
-    show: true,
-    backgroundColor: '#ffffff'
-  });
-  loginWindow.setMenuBarVisibility(false);
-  loginWindow.loadURL(`file://${__dirname}/login.html`);
+    loginWindow = new BrowserWindow({
+        webPreferences: { nodeIntegration: true },
+        parent: mainWindow, modal: true,
+        width: 300, height: 280,
+        frame: false,
+        show: true,
+        backgroundColor: '#ffffff'
+    });
+    loginWindow.setMenuBarVisibility(false);
+    loginWindow.loadURL(`file://${__dirname}/login.html`);
+    //loginWindow.webContents.openDevTools();
 });
 
 
 ipcMain.on('login-window-quit', (event, arg) => {
-  app.quit();
+    app.quit();
 })
+
 
 ipcMain.on('login-window-login', (event, arg) => {
-  mainWindow.webContents.send('login-try', arg);
+    mainWindow.webContents.send('login-try', arg);
 })
+
 
 ipcMain.on('login-success', (event, arg) => {
-  loginWindow.webContents.send('login-success', arg);
-  loginWindow.hide();
-  mainWindow.show();
+    loginWindow.webContents.send('login-success', arg);
+    loginWindow.hide();
+    mainWindow.show();
 })
+
 
 ipcMain.on('login-failed', (event, arg) => {
-  loginWindow.webContents.send('login-failed', arg);
+    loginWindow.webContents.send('login-failed', arg);
 })
 
+
 ipcMain.on('logout', (event, arg) => {
-  mainWindow.hide();
-  loginWindow.show();
+    mainWindow.hide();
+    loginWindow.show();
 })
